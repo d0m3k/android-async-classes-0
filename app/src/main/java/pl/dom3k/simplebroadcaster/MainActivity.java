d18db9e949db2d9858be84045f666fa744daeded
Dominik Adamiak <me@dom3k.pl>
2017-12-11 22:03:29 +0100

    Cursors

--- app/src/main/java/pl/dom3k/simplebroadcaster/MainActivity.java
+++ app/src/main/java/pl/dom3k/simplebroadcaster/MainActivity.java
@@ -5,6 +5,9 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import static android.net.ConnectivityManager.CONNECTIVITY_ACTION;
 
@@ -16,6 +19,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 //        TODO - we could register some events in here, for instance.
+
+
+
+//        TODO – do some actions regarding showing data from Content Provider:
+        findViewById(R.id.fetch_cursor).setOnClickListener(new View.OnClickListener() {
+            TextView textToAdd = findViewById(R.id.content_provider_text);
+
+            @Override
+            public void onClick(View view) {
+//                TODO – do something to get the cursor, and (possibly) its first element
+            }
+        });
+
+        findViewById(R.id.next_cursor).setOnClickListener(new View.OnClickListener() {
+            TextView textToAdd = findViewById(R.id.content_provider_text);
+
+            @Override
+            public void onClick(View view) {
+//                TODO – fetch next element in cursor or inform about its end, when comes.
+            }
+        });
     }
 
     @Override
