b4aa8f664686b71ffecfa32c56c88263ab2b58f8
Dominik Adamiak <me@dom3k.pl>
2017-12-12 10:35:35 +0100

    Broadcast1 changes

--- app/src/main/java/pl/dom3k/simplebroadcaster/MainActivity.java
+++ app/src/main/java/pl/dom3k/simplebroadcaster/MainActivity.java
@@ -10,28 +10,16 @@ import static android.net.ConnectivityManager.CONNECTIVITY_ACTION;
 
 public class MainActivity extends AppCompatActivity {
 
-    BroadcastReceiver br = new MyBroadcastReceiver();
-    BroadcastReceiver br2 = new NotifyBroadcastReceiver();
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        IntentFilter filter = new IntentFilter(CONNECTIVITY_ACTION);
-        filter.addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED);
-        this.registerReceiver(br, filter);
-
-        IntentFilter filter2 = new IntentFilter("pl.dom3k.broadcaster2.broadcast.MY_NOTIFICATION");
-        this.registerReceiver(br2, filter2);
     }
 
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-
-        unregisterReceiver(br);
-        unregisterReceiver(br2);
     }
 }
